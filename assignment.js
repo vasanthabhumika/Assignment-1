4edfacbb6ffb57b785b39d79fca150acf38a532e
vasanthabhumika <vasanthabhumika48@gmail.com>
2022-11-24 22:43:23 +0530

    Assignment - 2  Q/A

--- assignment.js
+++ assignment.js
@@ -1,1 +1,25 @@
-hi
+const task3Element = document.getElementById('task-3');
+
+function userName(){
+    alert("Hi there !!!");
+}
+
+userName();
+
+
+function userNameTwo(name){
+    alert(`Hi my name is ${name}`);
+}
+
+userNameTwo("Bhumika");
+
+task3Element.addEventListener("click", userName);
+
+
+function brandNew(param1, param2, param3){
+    const combinedString = `${param1} ${param2} ${param3}`;
+    return combinedString;
+}
+
+ const combinedText = brandNew(" Apoo", " Aishu", " Varshu");
+ alert(combinedText)
